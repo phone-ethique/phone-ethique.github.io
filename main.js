31a508ac68a99bacd34fa2625b66c0763a2aeafc
Valentin Stark <valentin@medialsace.fr>
2024-12-16 07:30:58 +0100

    ajout panier

--- main.js
+++ main.js
@@ -33,10 +33,34 @@ except.addEventListener("click", function (ev) {
     ev.stopPropagation(); //this is important! If removed, you'll get both alerts
 }, false);
 
+
 const langue = document.querySelector(".langue") ;
 langue.addEventListener("click", function(e) {
     e.preventDefault()
     body.style.display = "block"
+}, false)
+
+
+var bodypanier = document.getElementById('panierdehors');
+var exceptpanier = document.getElementById('bla');
+
+bodypanier.addEventListener("click", function () {
+    bodypanier.style.display = "none"
+}, false);
+exceptpanier.addEventListener("click", function (ev) {
+    ev.stopPropagation(); //this is important! If removed, you'll get both alerts
+}, false);
+
+const panier = document.querySelector(".panier") ;
+panier.addEventListener("click", function(e) {
+    e.preventDefault()
+    bodypanier.style.display = "block"
+}, false)
+
+var croix = document.querySelector("#croix")
+croix.addEventListener("click", function(e) {
+    e.preventDefault()
+    bodypanier.style.display = "none"
 }, false)
 
 });
