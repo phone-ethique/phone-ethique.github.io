2a560a1d4695dfe037344e649a098bb66e1acaf4
Valentin Stark <valentin@medialsace.fr>
2024-12-16 08:58:37 +0100

    maj panier

--- main.js
+++ main.js
@@ -63,6 +63,12 @@ croix.addEventListener("click", function(e) {
     bodypanier.style.display = "none"
 }, false)
 
+
+
+
+// add_article('coque-eco-phone')
+refresh_cart()
+
 });
 
 
@@ -72,3 +78,149 @@ function updatelang() {
     localStorage.setItem("lang", localisationValue)
     window.location.reload();
 }
+
+var articles = {}
+var total_articles = 0
+
+
+function init_cart() {
+    articles = JSON.parse(localStorage.getItem('articles'))
+    if (!articles) {
+        articles = {
+            'green-phone': {
+                'name': 'Green Phone',
+                'price': 388.99,
+                "number": 0
+            },
+            'e-phone-fr': {
+                'name': 'E-Phone',
+                'price': 299.99,
+                "number": 0
+            },
+            'e-phone-as': {
+                'name': 'E-Phone',
+                'price': 259.99,
+                "number": 0
+            },
+            'phone-plus': {
+                'name': 'Phone Plus',
+                'price': 689.99,
+                "number": 0
+            },
+            's-phone': {
+                'name': 'S-Phone',
+                'price': 429.99,
+                "number": 0
+            },
+            'nea-phone': {
+                'name': 'Nea Phone',
+                'price': 579.99,
+                "number": 0
+            },
+            'eco-phone': {
+                'name': 'Eco+ Phone',
+                'price': 329.99,
+                "number": 0
+            },
+            'chargeur': {
+                'name': 'Chargeur',
+                'price': 49.99,
+                "number": 0
+            },
+            'coque-e-phone': {
+                'name': 'Coque E-Phone',
+                'price': 69.99,
+                "number": 0
+            },
+            'coque-green-phone': {
+                'name': 'Coque Green Phone',
+                'price': 69.99,
+                "number": 0
+            },
+            'coque-phone-plus': {
+                'name': 'Coque Phone plus',
+                'price': 69.99,
+                "number": 0
+            },
+            'coque-s-phone': {
+                'name': 'Coque S-Phone',
+                'price': 69.99,
+                "number": 0
+            },
+            'coque-nea-phone': {
+                'name': 'Coque Nea Phone',
+                'price': 69.99,
+                "number": 0
+            },
+            'coque-eco-phone': {
+                'name': 'Coque Eco+ Phone',
+                'price': 69.99,
+                "number": 0
+            },
+        }
+    }
+
+
+    for (const [key, value] of Object.entries(articles)) {
+        total_articles += value.number;
+    }
+
+
+    save_cart()
+}
+
+function save_cart(){
+    localStorage.setItem('articles', JSON.stringify(articles));
+}
+
+function new_article (name, price, number) {
+    const article = document.createElement("div");
+    const title = document.createElement("h2")
+    const price_text = document.createElement("p")
+    const input_number = document.createElement("input")
+
+    article.classList.add("article")
+    title.innerText = name
+    article.appendChild(title)
+    price_text.innerText = price
+    article.appendChild(price_text)
+    input_number.value = number
+    input_number.type="number"
+    article.appendChild(input_number)
+
+    var liste_panier = document.querySelector(".liste_panier")
+    liste_panier.appendChild(article)
+}
+
+function refresh_cart() {
+    var total_price = 0
+    for (const [key, value] of Object.entries(articles)) {
+        if (value.number) {
+            new_article (value.name, value.price, value.number)
+            total_price += value.price * value.number
+        }
+    }
+    document.querySelector("#priceqwe").innerText = total_price
+}
+
+function set_article(article_name, number) {
+    articles[article_name].number = number
+    refresh_cart()
+    save_cart()
+}
+
+function add_article(name) {
+    articles[name].number += 1
+    refresh_cart()
+    save_cart()
+}
+
+function remove_article(name) {
+    if (articles[name].number > 0) {
+        articles[name].number -= 1
+        refresh_cart()
+        save_cart()
+    }
+}
+
+init_cart()
