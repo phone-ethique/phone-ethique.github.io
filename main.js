fd2b967aa060f824df6d77224900afdfb7e3e010
Valentin Stark <valentin@medialsace.fr>
2024-12-16 06:15:35 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+const lang = localStorage.getItem("lang");
+if (lang == "as") {
+    const as = document.querySelectorAll('.as');
+    as.forEach(element => {
+        element.style.display = "block"
+    });
+    
+    const fr = document.querySelectorAll(".fr") ;
+    console.log(fr)
+    fr.forEach(element => {
+        console.log(element)
+        element.style.display = "none"
+    });
+
+    const lang_text = document.querySelector(".langue p")
+    lang_text.innerText = "AS"
+}
+
+
+var body = document.getElementById('lang');
+var except = document.getElementById('inside');
+
+body.addEventListener("click", function () {
+    body.style.display = "none"
+}, false);
+except.addEventListener("click", function (ev) {
+    ev.stopPropagation(); //this is important! If removed, you'll get both alerts
+}, false);
+
+const langue = document.querySelector(".langue") ;
+langue.addEventListener("click", function(e) {
+    e.preventDefault()
+    body.style.display = "block"
+}, false)
+
+});
+
+
+function updatelang() {
+    var localisation = document.getElementById("localisation");
+    var localisationValue = localisation.options[localisation.selectedIndex].value;
+    localStorage.setItem("lang", localisationValue)
+    window.location.reload();
+}
